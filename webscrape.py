b25013744f5228e0a1479cbc56f80e6728a71866
Alexander Tingling <g.alexander.tingling@gmail.com>
2021-11-06 17:52:37 -0400

    Switched to Firefox

--- webscrape.py
+++ webscrape.py
@@ -1,14 +1,23 @@
 from selenium import webdriver
 
-driver = webdriver.Chrome(executable_path=r"chromedriver.exe")
+driver = webdriver.Firefox(executable_path='geckodriver')
 driver.get("https://benjaminbenben.com/lastfm-to-csv/")
 
 elem = driver.find_element_by_name('lastfm-user')
 elem.clear()
 elem.send_keys('alexanderting')
 
-driver.find_element_by_css_selector('btn btn-primary btn-lg').click()
-driver.wait(10)
+button = driver.find_element_by_xpath("/html/body/div/form/button")
+button.click()
+
+flag = True
+while flag:
+    current_sheet = driver.find_element_by_xpath('/html/body/div/section/h1').text
+    if current_sheet == 'finished':
+        flag = False
+
+save = driver.find_element_by_xpath('/html/body/div/section/p[2]/a[1]')
+save.click()
 
 driver.close()
 driver.quit()
